5bf04f53580c0c99a8750a009409aacc24ac2e68
Vegard Nyquist Borgeraas <vegabo@ulrik.uio.no>
2026-09-23 08:08:46 +0200

    cpmlited task 3b

--- IN3190 - Digital Signal Processing/task_3b.py
+++ IN3190 - Digital Signal Processing/task_3b.py
@@ -5,9 +5,16 @@ import matplotlib.dates as mdates
 from datetime import datetime, timezone
 import matplotlib.dates as mdates
 import numpy as np
+from scipy.optimize import curve_fit
+
+
+# regression moddel for curve_fit
+def model(t, celerity):
+    return celerity * t
+
 
 station_indices = []
-distances_km = []
+distances = []
 arrival_times = []
 
 with open("arrival_picks.csv", mode="r", newline="") as f:
@@ -16,20 +23,45 @@ with open("arrival_picks.csv", mode="r", newline="") as f:
 
     for row in reader:
         station_indices.append(int(row[0]))
-        distances_km.append(float(row[1]))
+        distances.append(float(row[1]) * 1000)
         arrival_times.append(float(row[2]))
 
+station_indices = np.array(station_indices)
+distances = np.array(distances)
 
 # Hunga Tonga major explosion onset (UTC)
 t_eruption = datetime(2022, 1, 15, 4, 15, 0, tzinfo=timezone.utc)
+travel_time_seconds = (arrival_times - mdates.date2num(t_eruption)) * 86400.0
 
-# Convert the float from the CSV back to a timezone-aware datetime object
-t_arrival = mdates.num2date(arrival_times)
+outliers = travel_time_seconds < 1
 
-# Compute elapsed travel time in seconds
-travel_time_seconds = (
-    np.array([(t - t_eruption).total_seconds() for t in t_arrival]) / 3600
+plt.scatter(
+    travel_time_seconds[~outliers],
+    distances[~outliers],
+    color="blue",
+    marker=".",
+    label="Valid picks",
 )
-plt.scatter(travel_time_seconds, distances_km)
+plt.scatter(
+    travel_time_seconds[outliers],
+    distances[outliers],
+    color="red",
+    marker="x",
+    label="Outliers",
+)
 
+celerity = curve_fit(model, travel_time_seconds[~outliers], distances[~outliers])[0][0]
+print(f"estimated celerity:{celerity:.2f}m/s")
+t = np.linspace(0, np.max(travel_time_seconds))
+plt.plot(
+    t,
+    model(t, celerity),
+    color="black",
+    label=f"Celerity regression ({celerity:.2f}m/s)",
+)
+
+plt.xlabel("Travel Time (seconds)")
+plt.ylabel("Distance (m)")
+plt.legend()
+plt.show()
 plt.show()
